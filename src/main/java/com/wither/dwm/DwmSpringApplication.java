73920f1e21506978e61db5363a564a5491633252
SZU-IC <979523179@qq.com>
2024-07-31 10:56:26 +0800

    代码生成器测试

---
+++ src/main/java/com/wither/dwm/DwmSpringApplication.java
@@ -0,0 +1,11 @@
+package com.wither.dwm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwmSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DwmSpringApplication.class);
+    }
+}
